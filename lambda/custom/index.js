0485d6f21689149eb27384949424cfe73bfa794e
Jeff Blankenburg <github@jeffblankenburg.com>
2019-07-09 14:37:27 -0400

    Starting the intent achievements.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -5,6 +5,7 @@ const Alexa = require("ask-sdk-core");
 
 const AchievementSound = "<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_02'/>"
 var AchievementSpeech = "";
+var AchievementCount = 0;
 var sessionCounter = 0;
 
 const LaunchRequestHandler = {
@@ -28,10 +29,11 @@ const HelloWorldIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent";
   },
   handle(handlerInput) {
-    const speechText = "Hello World!";
+    const speechText = AchievementSpeech + " Hello World!";
 
     return handlerInput.responseBuilder
       .speak(speechText)
+      .reprompt(speechText)
       .withSimpleCard("Hello World", speechText)
       .getResponse();
   },
@@ -97,14 +99,43 @@ const ErrorHandler = {
 
 function CheckForAchievements(handlerInput)
 {
+  console.log("CHECKING FOR ACHIEVEMENTS");
+  AchievementSpeech = "";
+  AchievementCount = 0;
   CheckSessionAchievements(handlerInput);
   CheckRequestAchievements(handlerInput);
   CheckDeviceAchievements(handlerInput);
   CheckIntentAchievements(handlerInput);
+  if (AchievementCount > 5) RemoveSounds(handlerInput)
 }
 
 function CheckSessionAchievements(handlerInput)
 {
+  console.log("CHECKING SESSION ACHIEVEMENTS");
+  const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+  if (handlerInput.requestEnvelope.session.new === true) {
+    sessionAttributes.requestCount = 1;
+  }
+  else {
+    sessionAttributes.requestCount++;
+  }
+
+  //FIVE REQUESTS IN ONE SESSION.
+  if (sessionAttributes.requestCount >= 5) createAchievement("You talked with this skill five times in one session.");
+
+  //TEN REQUESTS IN ONE SESSION.
+  if (sessionAttributes.requestCount >= 10) createAchievement("You talked with this skill ten times in one session.");
+
+  //TWENTY-FIVE REQUESTS IN ONE SESSION.
+  if (sessionAttributes.requestCount >= 25) createAchievement("You talked with this skill 25 times in one session.");
+
+  //FIFTY REQUESTS IN ONE SESSION.
+  if (sessionAttributes.requestCount >= 50) createAchievement("You talked with this skill fifty times in one session.");
+
+  //ONE HUNDRED REQUESTS IN ONE SESSION.
+  if (sessionAttributes.requestCount >= 100) createAchievement("You talked with this skill one hundred times in one session.");
+
   //STARTED FIRST SESSION.
 
   //FIFTH SESSION.
@@ -116,20 +147,11 @@ function CheckSessionAchievements(handlerInput)
   //FIFTIETH SESSION.
 
   //ONE HUNDRETH SESSION.
-
-  //FIVE REQUESTS IN ONE SESSION.
-
-  //TEN REQUESTS IN ONE SESSION.
-
-  //TWENTY-FIVE REQUESTS IN ONE SESSION.
-
-  //FIFTY REQUESTS IN ONE SESSION.
-
-  //ONE HUNDRED REQUESTS IN ONE SESSION.
 }
 
 function CheckRequestAchievements(handlerInput)
 {
+  console.log("CHECKING REQUEST ACHIEVEMENTS");
   //FIVE TOTAL REQUESTS
 
   //TEN TOTAL REQUESTS
@@ -147,6 +169,7 @@ function CheckRequestAchievements(handlerInput)
 
 function CheckDeviceAchievements(handlerInput)
 {
+  console.log("CHECKING DEVICE ACHIEVEMENTS");
   //SECOND DEVICE
 
   //THIRD DEVICE
@@ -160,22 +183,35 @@ function CheckDeviceAchievements(handlerInput)
 
 function CheckIntentAchievements(handlerInput)
 {
-  if (handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent")
-  {
-    createAchievement("Achievement Unlocked: You asked for help.")
+  if (handlerInput.requestEnvelope.request.type === "IntentRequest") {
+    if (1 === 2) {}
+    else if (handlerInput.requestEnvelope.request.intent.name === "AMAZON.CancelIntent") createAchievement("You said cancel.");
+    else if (handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent") createAchievement("You asked for help.");
+    else if (handlerInput.requestEnvelope.request.intent.name === "AMAZON.NavigateHomeIntent") createAchievement("You asked to navigate home.");
+    else if (handlerInput.requestEnvelope.request.intent.name === "AMAZON.StopIntent") createAchievement("You said stop.");
+    else if (handlerInput.requestEnvelope.request.intent.name === "AmazonActorIntent") createAchievement("You said the name of an actor.");
   }
 }
 
 function createAchievement(speechText)
 {
-  AchievementSpeech += AchievementSound + speechText;
+  console.log("CREATING ACHIEVEMENT");
+  AchievementSpeech += AchievementSound + "Achievement Unlocked: " + speechText + " ";
+  AchievementCount++;
   //TODO: RECORD THIS ACHIEVEMENT SOMEWHERE.
+}
+
+function RemoveSounds()
+{
+  console.log("REMOVING SOUNDS")
+  AchievementSpeech = AchievementSpeech.split(AchievementSound).join("");
+  AchievementSpeech = AchievementSound + AchievementSpeech;
 }
 
 const RequestLog = {
   async process(handlerInput) {
-    CheckForAchievements(handlerInput);
     console.log("REQUEST ENVELOPE = " + JSON.stringify(handlerInput.requestEnvelope));
+    CheckForAchievements(handlerInput);
     return;
   }
 };
